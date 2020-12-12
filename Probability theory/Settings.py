b41106548bc69383757aa8bddf70a1fb9a82a466
topjesus <62575393+topjesus@users.noreply.github.com>
2020-12-12 17:44:42 +0300

    Add files via upload

---
+++ Probability theory/Settings.py
@@ -0,0 +1,12 @@
+import numpy as np
+import sympy as sp
+import pandas as pd
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+import plotly.io as pio
+import plotly.express as px
+
+mpl.style.use('dark_background')
+pio.templates.default = 'plotly_dark'
